1f4e656b93a5172e945c0e1ecfc488947826e9a5
Wangjw6 <sysuwjw@gmail.com>
2025-03-29 10:28:02 +0900

    update the experiment entry

--- generate.py
+++ generate.py
@@ -1,16 +1,16 @@
-from simulator.engine.openai_api import OpenAIGPTAPI as LLM
-import pickle
-from simulator.engine.functions.traj_infer import *
-from simulator.engine.functions.PISC import *
-from simulator.engine.person_anonymized import *
+from engine.llm_configs.openai_api import OpenAIGPTAPI as LLM
+from engine.trajectory_generate import *
+from engine.persona_identify import *
+from engine.agent import *
 import argparse
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str,
-                    default='2019')  # 2019 for data only available in 2019, 2021 for data only available in 2021, 1921 for data available in both 2019 and 2021
+                    default='2019')  # 2019 for data only available in 2019, 2021 for data only available in 2021, 20192021 for data available in both 2019 and 2021
 parser.add_argument('--mode', type=int,
                     default=0)  # mode = 0 for learning based retrieval, 1 for evolving based retrieval
-parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--seed', type=int, default=123)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -22,31 +22,35 @@ if __name__ == "__main__":
                      5449, 5551, 573, 5765, 606, 6144, 6157, 6249, 638, 6581, 6615, 6670, 6814, 6863, 6973, 6998, 7228,
                      7259, 835, 934]
     available2019 = [2575, 1481, 1784, 2721, 638, 7626, 1626, 7266, 1568, 2078, 2610, 1908, 2683, 1883, 3637, 225, 914,
-                     6863, 6670, 323, 3282, 2390, 2337, 4396, 7259, 1310, 5849, 3802, 1522, 1219, 1004, 4105, 540, 827,
+                     6863, 6670, 323, 3282, 2390, 2337, 4396, 7259, 1310, 3802, 1522, 1219, 1004, 4105, 540,
                      6157, 1556, 2266, 13, 1874, 317, 2513, 3255, 934, 3599, 1775, 606, 3033, 3784, 5252, 3365, 6581,
                      6171, 5326, 2831, 3453, 3781, 2402, 4843, 439, 1172, 3501, 1032, 2542, 1184, 1531, 6615, 7228,
-                     1492,
-                     4987, 6204, 6693, 6973, 4057, 67, 2680, 2956, 3138, 3638, 5765, 835, 1431, 6249, 6998, 573, 884,
+                     1492 , 6973, 67, 2680, 2956, 3138, 3638, 5765, 835, 1431, 6249, 6998, 573, 884,
                      2356, 6463, 930, 3534, 6814, 5551, 5449, 6144, 6156, 4768, 2620, 4007, 1974]
-    available2021 = [1481, 1784, 2721, 638, 7626, 13, 47, 107, 225, 323, 392, 413, 439, 540, 572, 606, 638, 643, 789,
+    available2021 = [1481, 1784, 2721, 638,  7626, 13,   47,    107, 225,  323,  392,  413,  439,  540,  572, 606, 638, 643, 789,
                      1032, 1172, 1345, 1481, 1503, 1556, 1568, 1626, 1745, 1775, 6863, 7015, 7068, 7626, 7936,
                      1784, 1874, 1883, 1920, 2078, 2337, 2482, 2513, 2610, 2650, 2721, 2956, 3282, 3494, 3599, 3638,
                      3656, 4105, 4396, 4768, 4947, 5106, 5252, 5326, 6027, 6144, 6204, 6581, 6697, 7982]
-    folder = f"./database/{args.dataset}/"
-    data = {"2019": available2019, "2021": available2021, "1921": available1921}
+    folder = f"./data/{args.dataset}/"
+    data = {"2019": available2019, "2021": available2021, "20192021": available1921}
+    scenario_tag = {
+        '2019': 'normal',
+        '2021': 'abnormal',
+        '20192021': 'normal_abnormal'
+    }
     for k in data[args.dataset]:
         with open(folder + str(k) + ".pkl", "rb") as f:
             att = pickle.load(f)
-            P = Person(k, LLM())
-            P.train_routine_list, P.test_routine_list, P.attribute, P.activity_cat, P.cat, P.domain_knowledge, P.neg_routines, P.activity_area, P.area_freq, P.full_area, P.first_split, P.loc_cat = \
-            att[0], att[1], att[2], att[3], att[4], att[5], att[6], att[7], att[8], att[9], att[10], att[11]
+            P = Person(name=k, model=LLM(), person_id=k)
+            P.train_routine_list, P.test_routine_list, P.attribute, P.cat, P.domain_knowledge, P.neg_routines, P.activity_area, P.area_freq,  P.loc_cat = \
+                att[0], att[1], att[2],  att[4], att[5], att[6], att[7], att[8], att[11]
 
-        # identify the pattern of the person
-        P = pisc(P)
-        # initialize the retriever
-        P.init_retriever()
-        # activity generation
-        plan_new_day(P)
-        print("proved")
+        # identify the pattern of the person based on self-consistency
+        P = identify(P)
+        # # initialize the retriever
+        if args.mode == 0:
+            P.init_retriever()
+        # mobility generation
+        mob_gen(P, mode=args.mode, scenario_tag=scenario_tag[args.dataset])
 
     print("done")
